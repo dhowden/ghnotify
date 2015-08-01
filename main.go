419ca74cc60470e52f49ea7ae69db29787a57bf1
David Howden <dhowden@gmail.com>
2015-08-01 11:36:53 +1000

    Initial commit

---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"time"
+)
+
+var (
+	configFile string
+)
+
+var (
+	limitHeaderLimit     string = "X-Ratelimit-Limit"
+	limitHeaderRemaining        = "X-Ratelimit-Remaining"
+	limitHeaderReset            = "X-Ratelimit-Reset"
+)
+
+type Config struct {
+	Repos    []string `json:"repos"`
+	DataFile string   `json:"data_file"`
+	MinPoll  string   `json:"min_poll",omitempty`
+}
+
+func init() {
+	flag.StringVar(&configFile, "config", "config.json", "Config file")
+}
+
+func main() {
+	flag.Parse()
+
+	if configFile == "" {
+		fmt.Fprintf(os.Stderr, "no config specified\n")
+		os.Exit(1)
+		return
+	}
+
+	config, err := readConfig(configFile)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error reading %s: %s\n", configFile, err)
+		os.Exit(1)
+		return
+	}
+
+	repoList := config.Repos
+	if len(repoList) == 0 {
+		fmt.Fprintf(os.Stderr, "no repos specified\n")
+		os.Exit(1)
+		return
+	}
+
+	var minPoll time.Duration
+	if config.MinPoll != "" {
+		minPoll, err = time.ParseDuration(config.MinPoll)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "could not parse min_poll")
+			os.Exit(1)
+			return
+		}
+	}
+
+	poller := githubPoller{
+		repolist: repoList,
+		repos:    make(map[string]time.Time, len(repoList)),
+		minPoll:  minPoll,
+	}
+	poller.poll()
+}
+
+func readConfig(fileName string) (c Config, err error) {
+	f, err := os.Open(configFile)
+	if err != nil {
+		return
+	}
+	defer f.Close()
+
+	data, err := ioutil.ReadAll(f)
+	if err != nil {
+		return
+	}
+
+	err = json.Unmarshal(data, &c)
+	return
+}
---
+++ poller.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+type githubPoller struct {
+	remaining, limit int
+	reset            time.Time
+	minPoll          time.Duration
+
+	repolist []string
+	repos    map[string]time.Time
+}
+
+func (g *githubPoller) poll() {
+	timeout := time.After(0 * time.Second)
+	for {
+		select {
+		case <-timeout:
+			for _, repo := range g.repolist {
+				u, err := g.fetchRepoUpdated(repo)
+				if err != nil {
+					fmt.Println(err)
+				}
+				g.repos[repo] = u
+			}
+
+			fmt.Printf("Fetched repo data:\n%+v\n", g.repos)
+
+			polls := (g.remaining) / len(g.repolist)
+			r := g.reset.Sub(time.Now())
+			if polls > 0 {
+				rerun := time.Duration(int64(r) / int64(polls))
+				if g.minPoll > rerun {
+					rerun = g.minPoll
+				}
+				timeout = time.After(rerun)
+				fmt.Printf("Fetched repo data:\n%+v\nPoll delay: %v\n", g.repos, rerun)
+			} else {
+				fmt.Println("None left before next reset!")
+				timeout = time.After(g.reset.Sub(time.Now()))
+			}
+		}
+	}
+}
+
+func (g *githubPoller) fetchRepoUpdated(repo string) (result time.Time, err error) {
+	r, err := http.NewRequest("GET", "https://api.github.com/repos/"+repo, nil)
+	if err != nil {
+		return
+	}
+	r.Header.Add("Accept", "application/vnd.github.v3+json")
+
+	c := &http.Client{}
+	resp, err := c.Do(r)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	err = g.getHeaderInfo(resp.Header)
+	if err != nil {
+		return
+	}
+
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+	result, err = getUpdatedAt(bodyBytes)
+	return
+}
+
+func (g *githubPoller) getHeaderInfo(h http.Header) (err error) {
+	limit, err := strconv.Atoi(h.Get(limitHeaderLimit))
+	if err != nil {
+		return
+	}
+	g.limit = int(limit)
+
+	remaining, err := strconv.Atoi(h.Get(limitHeaderRemaining))
+	if err != nil {
+		return
+	}
+	g.remaining = int(remaining)
+
+	reset, err := strconv.ParseInt(h.Get(limitHeaderReset), 10, 0)
+	if err != nil {
+		return
+	}
+	g.reset = time.Unix(reset, 0)
+	return
+}
+
+func getUpdatedAt(data []byte) (result time.Time, err error) {
+	resp := make(map[string]interface{})
+	err = json.Unmarshal(data, &resp)
+	if err != nil {
+		return
+	}
+
+	u, ok := resp["updated_at"]
+	if !ok {
+		err = fmt.Errorf("`updated_at` not in returned resp")
+		return
+	}
+
+	us, ok := u.(string)
+	if !ok {
+		err = fmt.Errorf("expected `updated_at` to be a string, got %+v", u)
+		return
+	}
+
+	result, err = time.Parse(time.RFC3339, us)
+	return
+}
